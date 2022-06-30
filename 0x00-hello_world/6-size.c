9400ced100e052f8ee990fddfb689740ed244254
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-06-30 06:15:40 -0700

    Another update on sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,15 @@
  */
 int main(void)
 {
-	printf("Size of a char: ", sizeof(char charSize), " byte(s)\n");
-	printf("Size of an int: ", sizeof(int intSize), " byte(s)\n");
-	printf("Size of a long int: ", sizeof(long longSize), " byte(s)\n");
-	printf("Size of a long long int: ", sizeof(long long longLong), " byte(s)\n");
-	printf("Size of a float: ", sizeof(float floatSize), " byte(s)\n");
+	char charSize;
+	int intSize;
+	long longSize;
+	long long longLongSize;
+	float floatSize;
+	printf("Size of a char: ", sizeof(charSize), " byte(s)\n");
+	printf("Size of an int: ", sizeof(intSize), " byte(s)\n");
+	printf("Size of a long int: ", sizeof(longSize), " byte(s)\n");
+	printf("Size of a long long int: ", sizeof(longLongSize), " byte(s)\n");
+	printf("Size of a float: ", sizeof(floatSize), " byte(s)\n");
 	return (0);
 }
