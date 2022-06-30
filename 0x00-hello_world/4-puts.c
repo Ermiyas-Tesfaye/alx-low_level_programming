d37860a76bf5bb64c18977a8fb626e5291f24f0e
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-06-30 05:09:56 -0700

    Description for the main function is added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main function: a function thet print some text
+ * Return: always 0 will be returned
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
