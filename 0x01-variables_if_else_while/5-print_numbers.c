1f44422814b502c0ca2532bc6c8d1fa84d7b13b6
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-03 02:42:00 -0700

    All decimal numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  main - Prints alphabet in lowercase letters
+ *
+ *  Return: Always (Success)
+ */
+int main(void)
+{
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	printf("\n");
+	return (0);
+}
