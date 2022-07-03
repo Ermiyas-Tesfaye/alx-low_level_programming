00c45179b8c5be5fe57d8bc7244f4748be80bb7c
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-03 06:57:14 -0700

    putchar() four time

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  main - Prints all decimal numbers for 0 to 9
+ *
+ *  Return: Always (Success)
+ */
+int main(void)
+{
+	int x = '0';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		putchar(',');
+		putchar(' ');
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
