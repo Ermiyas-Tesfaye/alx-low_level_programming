3d447a02ecf480767f4b19e8ce130b134af57971
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-03 02:43:56 -0700

    Update

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- *  main - Prints alphabet in lowercase letters
+ *  main - Prints all decimal numbers for 0 to 10
  *
  *  Return: Always (Success)
  */
 int main(void)
 {
-	for (i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 		putchar(i);
 	printf("\n");
 	return (0);
