f38cacb69bf61a2da9919c8ae99a4ae0afe3c7f6
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-03 06:44:17 -0700

    All hexa characters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  main - Prints all hexadecimal letters 
+ *
+ *  Return: Always (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	char str[] = "0123456789abcdef";
+
+	while (str[i])
+	{
+		putchar(str[i]);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
