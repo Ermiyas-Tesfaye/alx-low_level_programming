1798cc173dffdddb4dd0f31017160b56708d1f43
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-05 09:39:09 -0700

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_to_98 - print natural numbers
+ *
+ * @n: starting point
+ */
+void print_to_98(int n)
+{
+	int target = 98;
+
+	if (n < 98)
+	{
+		while (n != 98)
+		{
+			printf("%n, ", n);
+			n++
+		}
+		printf("%d", target);
+	}
+	else if (n > 98)
+	{
+		while (n != 98)
+		{
+			printf("%n, ", n);
+			n--;
+		}
+		printf("%d", target);
+	}
+	else
+		printf("%d", n)
+}
