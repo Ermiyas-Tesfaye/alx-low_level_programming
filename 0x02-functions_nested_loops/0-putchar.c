f4160c02c3642c337c6e32b48b8e41e253eec71b
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-05 02:59:02 -0700

    First test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * main - Prints string character
+ *
+ * REturn: Always 0 (Success)
+ */
+int main (void)
+{
+	int i = 0;
+	char str[] = "_putchar";
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (0);
+}
