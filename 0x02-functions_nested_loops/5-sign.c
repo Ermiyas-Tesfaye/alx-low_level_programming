b8d37c40c7228dc53e71dc9ae56b64e0c76d6443
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-05 06:40:32 -0700

    Betty

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign -print sign of the number
+ *
+ * @n: the number to be checked
+ *
+ * Return: 1 for positive 0 for zero and -1 for negitive
+ */
+int print_sign(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		i = 1;
+	}
+	if else (n < 0)
+	{
+		_putchar('-');
+		i = -1;
+	}
+	else
+		_putchar('0');
+	return (i);
+}
