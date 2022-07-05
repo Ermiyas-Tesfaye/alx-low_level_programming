5c676269d36211ebb7c15570d6616cd0e5ea6222
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-05 08:45:49 -0700

    Finaly I did it

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,27 +6,36 @@
  */
 void jack_bauer(void)
 {
-	int i = 0, j = 0, x = 0, y = 0;
+	char i = '0';
 
-	while (i < 3)
+	while (i < '3')
 	{
-		while (j < 10)
+		char j = '0';
+
+		while (j <= '9')
 		{
-			while (x < 7)
+			if (i != '2' || j != '4')
 			{
-				while (y < 10)
+				char x = '0';
+
+				while (x < '6')
 				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(':');
-					_putchar(x);
-					_putchar(y);
-					_putchar('\n');
-					y++;
+					char y = '0';
+
+					while (y <= '9')
+					{
+						_putchar(i);
+						_putchar(j);
+						_putchar(':');
+						_putchar(x);
+						_putchar(y);
+						_putchar('\n');
+						y++;
+					}
+					x++;
 				}
-				x++;
+				j++;
 			}
-			j++;
 		}
 		i++;
 	}
