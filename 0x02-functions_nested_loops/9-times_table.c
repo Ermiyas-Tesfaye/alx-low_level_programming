9e8362eaa1f5d8c32f6f03b2a768c1802defab43
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-05 22:37:06 -0700

    another way

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,34 +6,35 @@
  */
 void times_table(void)
 {
-	int rone = 0;
+	int i, j, res;
 
-	while (rone <= 9)
+	i = 0;
+	while (i <= 0)
 	{
-		int cone = 0;
-
-		while (cone <= 9)
+		j = 0;
+		while (j <= 9)
 		{
-			int d;
-
-			d = rone * cone;
-			if ((d / 10) > 0)
+			res = (i * j);
+			if (j != 0)
 			{
-				_putchar((d / 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (res >= 10)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else if (res < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar((res % 10) + '0');
 			}
 			else
-				_putchar(' ');
-			_putchar((d % 10) + '0');
-			if (cone == 0)
-				_putchar(',');
-			else if (cone < 9 && cone != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			cone++;
+				_putchar((res % 10) + '0');
+			j++;
 		}
 		_putchar('\n');
-		rone++;
+		i++;
 	}
 }
