2321ac7c4c6fc87114eda1d8d73e364ffa516913
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-06 08:48:27 -0700

    The 9th task is corrected

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,29 +9,29 @@ void times_table(void)
 	int i, j, res;
 
 	i = 0;
-	while (i <= 0)
+	while (i <= 9)
 	{
 		j = 0;
 		while (j <= 9)
 		{
 			res = (i * j);
-			if (j != 0)
+			if (res > 9)
+			{
+				_putchar(res / 10 + '0');
+				_putchar(res % 10 + '0');
+			}
+			else if (j != 0)
+			{
+				_putchar(' ');
+				_putchar(res + '0');
+			}
+			else
+				_putchar(z + '0');
+			if (j != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (res >= 10)
-			{
-				_putchar((res / 10) + '0');
-				_putchar((res % 10) + '0');
-			}
-			else if (res < 10 && j != 0)
-			{
-				_putchar(' ');
-				_putchar((res % 10) + '0');
-			}
-			else
-				_putchar((res % 10) + '0');
 			j++;
 		}
 		_putchar('\n');
