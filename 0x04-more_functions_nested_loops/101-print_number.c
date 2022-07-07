b7b09e0a4b7f532652bb38f61219c926b305b4c3
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-07 08:41:47 -0700

    Recall the function

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,10 +7,10 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n = -n;
 	}
 	if (n / 10)
-		_putchar(n / 10);
+		print_number(n / 10);
 	_putchar(n % 10 + '0');
 }
