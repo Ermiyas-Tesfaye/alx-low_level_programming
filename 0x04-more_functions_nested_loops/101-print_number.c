ae45918dceea9657c6c502105895c608f68e8b47
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-08 08:19:00 -0700

    New update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,10 +7,27 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		if (n < 10)
+		{
+			n = n % 10;
+			_putchar(n + '0');
+		}
+		else if (n < 100)
+		{	
+			int n1 = n / 10;
+			_putchar(n1 + '0');
+			n = n % 10;
+			_putchar(n + '0');
+		}
+		else if (n < 1000)
+		{
+			int n2 = n / 100;
+			_putchar(n2 + '0');
+			int n1 = (n / 10) % 10;
+			_putchar(n1 + '0');
+			n = n % 10;
+			_putchar(n + '0');
+		}	
 	}
-	if (n / 10)
-		print_number(n / 10);
 	_putchar(n % 10 + '0');
 }
