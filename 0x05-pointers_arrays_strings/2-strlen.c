0bb3bbcf8b4f62c9e922d3d8dc9327fd21754cda
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-11 04:56:29 -0700

    The second task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/**
+ * _strlen - returns the length of a string
+ * @s: the string value
+ * Return: lengeth of string
+ * Author: Ermiyas Tesfaye
+ */
+int _strlen(char *s)
+{
+	int n;
+	int i;
+
+	i = 0;
+	while (*s[i])
+	{
+		if (*s[i] != ' ')
+			n++;
+		i++;
+	}
+	return (n);
+}
