f93b84e1599c3e801282d6321ab11e4f16e0d468
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-11 09:02:34 -0700

    Final of this

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * _atoi - convert a string to an int
+ * _atoi - Convert char to int
  * @s: the string value
  * Author: Ermiyas Tesfaye
+ * Return: the integer value
  */
 int _atoi(char *s)
 {
