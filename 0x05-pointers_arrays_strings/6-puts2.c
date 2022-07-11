3fb08345704a6643950c754af29f183c36fce6cb
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-11 08:05:08 -0700

    Puts - print every even index chars

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every even index of a char
+ * @str: the string value
+ * Return: non return value
+ * Author: Ermiyas Tesfaye
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
