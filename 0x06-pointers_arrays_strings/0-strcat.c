1899b60bba41df1e835361a230ad66bb5af330a7
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-13 06:56:40 -0700

    New update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,21 +7,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char *new;
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\0')
+	while (dest[j] != '\0')
 	{
-		new[i] = dest[i];
 		i++;
 	}
 	while (src[j] != '\0')
 	{
-		new[i] = src[j];
+		dest[i] = src[j];
 		i++;
 		j++
 	}
-	new[i] = '\0';
-	return (new);
+	dest[i] = '\0';
+	return (dest);
 }
