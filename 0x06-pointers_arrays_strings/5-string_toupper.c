486d4ad9a4c80ebe86f1614b7283b473f1109112
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-14 04:50:22 -0700

    New up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,9 +7,13 @@
  */
 char *string_toupper(char *s)
 {
-	int length = strlen(s);
 	int i = 0;
-	
-	for (i = 0; i < length; i++)
-		s[i] = toupper(s[i]);
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
 }
