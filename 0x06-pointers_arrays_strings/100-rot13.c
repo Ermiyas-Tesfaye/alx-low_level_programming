a3805fac1b09213c9aeb5d7577d5190585c6d600
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-14 06:42:22 -0700

    rot 13 encryption

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - encrypts like Modified Cesar cipher
+ * @s: string to be converted
+ * Return: the encryped string
+ */
+char *rot13(char *s)
+{
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	int j;
+
+	while (s[i] != '\n')
+	{
+		for (j = 0; j <= 51; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = rot[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
