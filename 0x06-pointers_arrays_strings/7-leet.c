eb2ad92563074de942cfd466fcc604a6dfc59811
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-14 11:17:02 -0700

    Pointer is powerfull

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,19 +7,21 @@
 char *leet(char *s)
 {
 	int i = 0;
+	int j;
+	int l_c[] = {97, 101, 111, 116, 108};
+	int u_c[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
 
-	while (s[i] != '\0')
+	while (*(s + i) != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j < 5; j++)
+		{
+			if (*(s + i) == l_c[j] || *(s + i) === u_c[j])
+			{
+				*(s + i) = numbers[j];
+				break;
+			}
+		}
 		i++;
 	}
 	return (s);
