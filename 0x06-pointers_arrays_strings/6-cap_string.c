eb006f3e7bf992ade9ec98e926f99c5cab050c7a
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-14 05:39:22 -0700

    Capitalize each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * cap_string - capitalize all word of a string
+ * @s: string value
+ * Return: char value
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+	int j;
+	int ar[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	while (s[i] != '\0')
+	{
+		if (i == 0)
+			s[i] = s[i] - 32;
+		else
+		{
+			for (j = 0; j <= 12; j++)
+			{
+				if (ar[j] == s[i] - 1)
+					s[i] = s[i] - 32;
+			}
+		}
+	i++;
+	}
+	return (s);
+}
