009baca5b1243133b29304a68734aaa6c18dcad3
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-19 06:42:48 -0700

    UP

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,23 +8,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, c, d;
-	int x = size - 1;
+	int i, as, ds, c, d;
 
+	c = 0;
+	d = 0;
+	ds = size - 1;
 	for (i = 0; i < size; i++)
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-				c = c + a[i][j];
-		}
-	for (i = 0; i < size; i++)
-		for (j = 0; j < size; j++)
-		{
-			if (j == x)
-			{
-				d = d + a[i][x];
-				x--;
-			}
-		}
+	{
+		c += *(a + i * size + as);
+		d += *(a + i * size + ds);
+		as++;
+		ds--;
+	}
 	printf("%d, %d\n", c, d);
 }
