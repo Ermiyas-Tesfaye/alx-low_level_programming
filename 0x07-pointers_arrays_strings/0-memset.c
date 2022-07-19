5b19f1ae8e71ba62076cc266ab05a7844e5cc8a6
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-19 00:20:44 -0700

    new

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,9 +10,9 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		s[i] = b;
-	for( ; i < n; i++)
+	for ( ; i < n; i++)
 		s[i] = '\0';
 	return (s);
 }
