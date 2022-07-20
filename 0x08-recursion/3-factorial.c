e8d57a7038a318d67000653cef9c001de27b47e4
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-20 01:23:04 -0700

    Factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given number
+ * @n: the given number
+ * Return: the calculated facctorial
+ * Author: Ermiyas Tesfaye
+ */
+int factorial(int n)
+{
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
