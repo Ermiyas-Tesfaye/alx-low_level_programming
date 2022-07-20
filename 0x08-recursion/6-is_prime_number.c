8aa97adaf5144f4d2871734ba29b4be4707e06c2
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-20 05:19:31 -0700

    Nuw update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,7 +9,7 @@ int prime_checker(int n, int i)
 {
 	if (i == n)
 		return (0);
-	if (i % n == 0)
+	if (n % i == 0)
 		return (1);
 	return (prime_checker(n, i + 1));
 }
@@ -22,7 +22,7 @@ int is_prime_number(int n)
 {
 	int i = 2;
 
-	if (n < 1)
+	if (n <= 1)
 		return (0);
 	else if (n == 2)
 		return (1);
