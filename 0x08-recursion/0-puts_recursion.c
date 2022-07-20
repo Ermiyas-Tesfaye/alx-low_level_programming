fa96d28989b67efd5aee7452e05d44cbef6a3740
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-20 00:30:20 -0700

    Recursion putchar

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string followed by \n
+ * @s: string value to be print
+ * Author: Ermiyas Tesfaye
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_putchar(s + 1);
+}
