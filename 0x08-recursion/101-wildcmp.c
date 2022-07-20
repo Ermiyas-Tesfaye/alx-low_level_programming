72bf5339f1a58ac58cf61383df53dfbf08fb5522
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-20 07:09:16 -0700

    Update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,5 +7,15 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (1);
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wild(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
