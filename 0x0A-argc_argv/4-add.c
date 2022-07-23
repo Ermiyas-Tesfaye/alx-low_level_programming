18456a0c2722ddd38ad9ca569ed9d70b64ba78f3
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-23 01:08:01 -0700

    Finally i get the answer for forth question

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _isnumber - chacke for number
+ * @str: string value from argv
+ * @ptr: copy of argv value
+ * Return: 1 if no success on 0 for success
+ */
+int _isnumber(char *str)
+{
+	int i = 1;
+
+	while (*str != '\0')
+	{
+		if (!(*str >= '0' && *str <= '9'))
+		{
+			i = 0;
+		}
+		str++;
+	}
+	return (i);
+}
+/**
  * main - a program that adds positive numbers
  * @argc: argument count
  * @argv: argument vector
@@ -14,13 +34,15 @@ int main(int argc, char *argv[])
 		printf("0\n");
 	for (i = 1; i < argc; i++)
 	{
-		if ()
+		if (_isnumber(argv[i]))
 			sum += atoi(argv[i]);
 		else
 		{
-			printf("Error");
+			printf("Error\n");
 			return (1);
 		}
 	}
+	if (argc > 1)
+		printf("%d\n", sum);
 	return (0);
 }
