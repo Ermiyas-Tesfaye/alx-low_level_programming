26f11d71837117c47ddb9e3aa1fbfae4749ae865
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-23 04:14:09 -0700

    New

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,29 +8,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	char *x;
-	int i = 0;
-	int j = 0;
+	int l, i;
 
-	if (s[i] == '\0')
-		x[j] = s[i];
-	else if (s[i] != '\0')
+	l = 0;
+	while (s[l] != '\0')
+		l++;
+	for (i = 0; i <= l; i++, s++)
 	{
-		while (s[i] != '\0')
-		{
-			if (s[i] == c)
-			{
-				break;
-			}
-			i++;
-		}
-		while (s[i] != '\0')
-		{
-			x[j] = s[i];
-			i++;
-			j++;
-		}
-		x[j] = '\0';
+		if (*s == c)
+			return (s);
 	}
-	return (*x);
+	return (0);
 }
