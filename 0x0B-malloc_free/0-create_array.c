b7ef1bac4e41c9879a36a7d4067636493cdbf70e
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-24 02:08:50 -0700

    Betty code is fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,9 +10,14 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *ptr = (char*)malloc(size);
+	char *ptr = (char *)malloc(size);
 
-	for (i = 0; i < size; i++)
-		ptr[i] = c;
-	return (ptr);
+	if (size != 0)
+	{
+		for (i = 0; i < size; i++)
+			ptr[i] = c;
+		return (ptr);
+	}
+	else
+		return (NULL);
 }
