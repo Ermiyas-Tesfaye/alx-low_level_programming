3ba5495f8e608f26967163bfdf5d4786448135f9
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-24 03:13:46 -0700

    Update

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - return a pointer to a newly allocated space in memory
+ * @str: given string value
+ * Return: NULL if 0
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *ptr = malloc(sizeof(str));
+
+	if (str == NULL)
+		return (NULL);
+	else
+	{
+		for (i = 0; i < sizeof(str); i++)
+			ptr[i] = str[i];
+	}
+	ptr[i] = '\0';
+	free();
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: a new string or NULL value
+ * Author: Ermiyas
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	char *ptr = malloc(sizeof(s1) + sizeof(s2));
+
+	for (i = 0; i < (sizeof(s1) + sizeof(s2); i++)
+	{
+		while (*(s1) != '\0')
+			ptr[i] = *s1;
+		while (*(s2) != '\0')
+			ptr[i] = *s2;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
