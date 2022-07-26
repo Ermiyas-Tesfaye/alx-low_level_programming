71d15c73bc3db11733ade40b01084a39e2ce89d1
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-26 00:56:58 -0700

    Update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,26 +1,65 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * _size - returns the size of a string
+ * @s: string value
+ * Return: size
+ */
+int _size(char *s)
+{
+	int j = 0;
+	int i = 0;
+
+	while (*s != '\0' && *s != NULL)
+	{
+		j++;
+		i++;
+		s++;
+	}
+	while (i != 0)
+	{
+		i--;
+		s--;
+	}
+	return (j);
+}
+/**
  * str_concat - function that concatenates two strings
  * @s1: first string
  * @s2: second string
  * Return: a new string or NULL value
- * Author: Ermiyas
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i;
-	char *ptr = malloc(sizeof(s1) + sizeof(s2));
+	int i, j, x;
+	char *ptr;
 
-	for (i = 0; i < (sizeof(s1) + sizeof(s2)); i++)
+	i = _size(s1);
+	j = _size(s2);
+	ptr = (char *)malloc(i + j);
+	for (x = 0; x < (i + j); x++)
 	{
-		while (*(s1) != '\0')
-			ptr[i] = *s1;
-		while (*(s2) != '\0')
-			ptr[i] = *s2;
-		s1++;
-		s2++;
+		while (*s1 != '\0')
+		{	
+			if (*s1 == NULL)
+			{	
+				ptr[x] = "";
+				break;
+			}
+			ptr[x] = *s1;
+			s1++;
+		}
+		while (*s2 != '\0')
+		{	
+			if (*s2 == NULL)
+			{	
+				ptr[x] = "";
+				break;
+			}
+			ptr[x] = *s2;
+			s2++;
+		}
 	}
-	ptr[i] = '\0';
+	ptr[x] = '\0';
 	return (ptr);
 }
