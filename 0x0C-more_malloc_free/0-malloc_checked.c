94a1f67b65c0d4257db7f5654cf037445f400ab4
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-28 00:20:01 -0700

    First

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * malloc_checked - a function that allocates memory using malloc
+ * @b: size_t value of malloc
+ * Author: Ermiyas
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int i;
+	void *str = malloc(b);
+
+	if (str == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	for (i = 0; i < b; i++)
+	{
+		str[i] = '\0';
+	}
+	return (str);
+}
