2de1dcf86e238bd9ddd6f0422e34a0b56c75c84f
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-07-29 01:30:53 -0700

    UP

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,12 +2,13 @@
 /**
  * malloc_checked - a function that allocates memory using malloc
  * @b: size_t value of malloc
+ * Return: the pointer
  * Author: Ermiyas
  */
 void *malloc_checked(unsigned int b)
 {
 	unsigned int i;
-	void *str = malloc(b);
+	void *str = malloc(b + 1);
 
 	if (str == NULL)
 		exit(EXIT_FAILURE);
