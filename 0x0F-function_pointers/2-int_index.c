0e84593d2c9562e2e2a1a680bc1cb417505f37d4
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-08-03 06:19:35 -0700

    Hot Update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,12 +3,15 @@
  * int_index - a function that searches for an integer
  * @array: Given array
  * @size: number of element in array
+ * @cmp: function pointer
  * Return: the index of the first element
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
