7123e0ed330f9a8eb3b1d1c13af82bcae55e9a1e
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-08-11 07:31:33 -0700

    Code for constructor is added

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * myStart - print before the mainfunction
+ */
+void myStart(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
