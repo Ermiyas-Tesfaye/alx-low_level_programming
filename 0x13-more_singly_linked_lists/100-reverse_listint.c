6a12553da329910abacbd1e3601e57cacff6c6ed
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-08-16 06:24:21 -0700

    Reverse Order

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * reverse_listint - a function that reverse a linked list
+ * @head: apointerof poiter fir the first node
+ * Return: apointer to the first node
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *temp = NULL;
+	listint_t *temp2 = NULL;
+
+	while (*head)
+	{
+		temp2 = (*head)->next;
+		(*head)->next = temp;
+		temp = *head;
+		*head = temp2;
+	}
+	*head = temp;
+	return (*head);
+}
