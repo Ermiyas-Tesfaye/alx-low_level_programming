5785389fb8acf8e68de5f5c57b53f5cc36b61265
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-08-17 02:11:32 -0700

    New update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,32 +2,33 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * print_int - a fun.. that print integer value
- * @n: value to print
- *
-void print_int(int n)
-{
-	if (n > 9)
-		print_int(n / 10);
-	_putchar(n % 10 + '0');
-}
-*/
-/**
  * print_listint_safe - a fun.. that prints a linked list
  * @head: a pointer to the first node
  * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i = 0;
+	int difference, i, number;
+	void *address;
 
-	if (!head)
-		exit(98);
-	while (head)
-	{
-		printf("%d\n", head->n);
-		head = head->next;
-		i++;
-	}
-	return (i);
+	i = 0;
+
+				while (head != NULL)
+						{
+									difference = head - head->next;
+											i++;
+													number = head->n;
+															printf("[%p] %i\n", (void *)head, number);
+																	if (difference > 0)
+																					head = head->next;
+																			else
+																						{
+																										address = head->next;
+																													number = head->next->n;
+																																printf("-> [%p] %i\n", address, number);
+																																			break;
+																																					}
+
+																				}
+					return (i);
 }
