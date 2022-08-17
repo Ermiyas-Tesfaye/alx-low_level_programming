15e0871d31c4f9c9f1c33db02b4a72afda45a284
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-08-17 02:15:02 -0700

    New up

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,23 +12,21 @@ size_t print_listint_safe(const listint_t *head)
 	void *address;
 
 	i = 0;
-
-				while (head != NULL)
-						{
-									difference = head - head->next;
-											i++;
-													number = head->n;
-															printf("[%p] %i\n", (void *)head, number);
-																	if (difference > 0)
-																					head = head->next;
-																			else
-																						{
-																										address = head->next;
-																													number = head->next->n;
-																																printf("-> [%p] %i\n", address, number);
-																																			break;
-																																					}
-
-																				}
-					return (i);
+	while (head != NULL)
+	{
+		difference = head - head->next;
+		i++;
+		number = head->n;
+		printf("[%p] %i\n", (void *)head, number);
+		if (difference > 0)
+			head = head->next;
+		else
+		{
+			address = head->next;
+			number = head->next->n;
+			printf("-> [%p] %i\n", address, 98);
+			break;
+		}
+	}
+	return (i);
 }
