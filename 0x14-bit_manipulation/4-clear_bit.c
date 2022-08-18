8097db237d3b3b1b567402d4aec3ead864eee0d4
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-08-18 05:37:55 -0700

    Hot Fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,10 +7,8 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int temp = 1 << index;
-
 	if (index > 64)
 		return (-1);
-	*n = ~(1 << index);
+	*n &= ~(1 << index);
 	return (1);
 }
