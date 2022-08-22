1fa5b7369d1d360d4db56dab2f8ca4c13af89ff0
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-08-22 05:55:25 -0700

    New commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * create_file - a fun.. that creates a file
+ * @filename: the name of the file to create
+ * @text_content: a NULL turmenated string
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i;
+
+	for (i = 0; text_content[i]; i++)
+		;
+	i++;
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, i);
+	close(fd);
+	return (1);
+}
