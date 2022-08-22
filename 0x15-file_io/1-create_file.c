f3b02b0572f0ca1846b23a01df73e9c66bf1a2ef
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-08-22 07:35:52 -0700

    Hot Fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,6 @@ int create_file(const char *filename, char *text_content)
 		b_written = write(fd, text_content, i);
 	close(fd);
 	if (b_written < 0)
-		return (-1)
+		return (-1);
 	return (1);
 }
