f3d2dfc8c413a36fa5b9eadb450d187a9f39fd06
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-08-22 07:40:55 -0700

    Hot Fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, b_written = 0;
+	int fd;
 	int i;
 
 	for (i = 0; text_content[i]; i++)
@@ -17,10 +17,9 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content)
-		b_written = write(fd, text_content, i);
-	close(fd);
-	if (b_written < 0)
+	if (!text_content)
 		return (-1);
+	write(fd, text_content, i);
+	close(fd);
 	return (1);
 }
