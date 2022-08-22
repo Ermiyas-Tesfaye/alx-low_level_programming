8c173ad8840441dbe08b62b8e77e24785c4988d3
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-08-22 05:30:36 -0700

    Initial Commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * read_textfile - a fun.. that reads a text file and print it to POSIX
+ * @filename: file name
+ * @letters: size of the file in char
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	size_t i;
+	char *buff;
+	int actual_num = 0;
+
+	buff = (char *)malloc(letters + 1);
+	if (buff == NULL)
+		return (0);
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	read(fd, buff, letters);
+	buff[letters] = '\0';
+	for (i = 0; i < letters && buff[i] != '\0'; i++)
+	{
+		_putchar(buff[i]);
+		actual_num++;
+	}
+	close(fd);
+	free(buff);
+	return (actual_num);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,16 @@
+*** C - File I/O ***
+
+C provides a number of functions that helps to perform basic file operations.
+This includes:
+   Function	description
+* fopen()	create a new file or open a existing file
+* fclose()	closes a file
+* getc()	reads a character from a file
+* putc()	writes a character to a file
+* fscanf()	reads a set of data from a file
+* fprintf()	writes a set of data to a file
+* getw()	reads a integer from a file
+* putw()	writes a integer to a file
+* fseek()	set the position to desire point
+* ftell()	gives current position in the file
+* rewind()	set the position to the begining point
